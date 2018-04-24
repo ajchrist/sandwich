6b47316c51e32ec4315b03a76d1983f0ce32e4ea
Andrew Christiano <ajchristiano91@gmail.com>
2018-04-24 12:58:44 -0400

    first push

---
+++ sandwich-club-starter-code/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -0,0 +1,47 @@
+package com.udacity.sandwichclub.utils;
+
+import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonUtils {
+
+    public static Sandwich parseSandwichJson(String json) {
+        JSONObject sJson;
+        try {
+            sJson = new JSONObject(json);
+        } catch (JSONException e) {
+            return null;
+        }
+        String mainName;
+        List<String> alsoKnownAs;
+        String placeOfOrigin;
+        String description;
+        String image;
+        List<String> ingredients;
+        try {
+            mainName = sJson.getJSONObject("name").getString("mainName");
+            alsoKnownAs = fromJsonArray(sJson.getJSONObject("name").getJSONArray("alsoKnownAs"));
+            placeOfOrigin = sJson.getString("placeOfOrigin");
+            description = sJson.getString("description");
+            image = sJson.getString("image");
+            ingredients = fromJsonArray(sJson.getJSONArray("ingredients"));
+        } catch (JSONException e) {
+            return null;
+        }
+        return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+    }
+
+    private static List<String> fromJsonArray(JSONArray array) throws JSONException {
+        ArrayList<String> tempArr = new ArrayList<>();
+        for (int i = 0; i < array.length(); i++){
+            tempArr.add((String) array.get(i));
+        }
+        return tempArr;
+    }
+}
